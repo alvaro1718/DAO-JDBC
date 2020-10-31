423c40818b1d44be3cf717ccac1555f346235a7a
Álvaro Fonseca <vasilca52@gmail.com>
2020-10-31 17:16:27 +0000

    Criação da classe da entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 package application;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -14,5 +19,14 @@ public class Program {
 		dept1.setName("Test");
 		
 		System.out.println(dept1.toString());
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+		try {
+			Seller seller = new Seller(2,"ferros","ferros@ferros.pt",2344.0, new java.sql.Date(sdf.parse("11/12/1997").getTime()),dept1);
+			System.out.println(seller);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
