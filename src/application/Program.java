37363dba2114625fc53298104b89f43b7d203268
alvaro1718 <36928711+alvaro1718@users.noreply.github.com>
2020-11-01 13:59:49 +0000

    Implementação do método findById no SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,30 +13,9 @@ public class Program {
 
 	public static void main(String [] args) {
 		
-		Department dept1 = new Department(11,"RH");
-		
-		System.out.println(dept1.toString());
-		
-		dept1.setId(4);
-		dept1.setName("Test");
-		
-		System.out.println(dept1.toString());
-		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
-		String mydate = "11/04/1998 03:03:45";
-		
-		Seller seller;
-		
-		try {
-				seller = new Seller(2,"ferros","ferros@ferros.pt",2344.0, simpleDateFormat.parse(mydate),dept1);
-				System.out.println(seller);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
 		SellerDao sellerDao =	DaoFactory.createSellerDao();
-			
+		Seller seller = sellerDao.findById(2);
 		
+		System.out.println(seller.toString());
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller seller) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,42 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		Seller seller = new Seller();
+		Department department = new Department();
+				
+		try {
+			st = conn.prepareStatement("SELECT coursejdbc.seller.*, coursejdbc.department.Name as deptName FROM coursejdbc.seller inner join coursejdbc.department "
+					+ "on coursejdbc.seller.DepartmentId=coursejdbc.department.Id"
+					+ " where coursejdbc.seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				
+				seller.setId(rs.getInt("Id"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				department.setId(rs.getInt("DepartmentId"));
+				department.setName(rs.getString("deptName"));
+				seller.setDepartment(department);
+				
+				return seller;
+			}
+				return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
